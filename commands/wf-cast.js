34bf66f1ecbdbc3ce4d81a5d2bcceb0f989e2a05
gamassss <gamassakbar@gmail.com>
2023-01-28 21:48:57 +0700

    mapping wmo code to have interpretation value

--- commands/wf-cast.js
+++ commands/wf-cast.js
@@ -18,6 +18,37 @@ module.exports = {
 			return response.json();
 		}
 
+		const weather_map = new Map([
+			['0', 'Clear sky'],
+			['1', 'Mainly clear'],
+			['2', 'Partly cloudy'],
+			['3', 'Overcast'],
+			['45', 'Fog'],
+			['48', 'Depositing rime fog'],
+			['51', 'Drizzle light'],
+			['53', 'Drizzle moderate'],
+			['55', 'Drizzle dense intensity'],
+			['56', 'Freezing drizzle light'],
+			['57', 'Freezing drizzle dense intensity'],
+			['61', 'Rain slight'],
+			['63', 'Rain moderate'],
+			['65', 'Rain heavy intensity'],
+			['66', 'Freezing rain light'],
+			['67', 'Freezing rain heavy intensity'],
+			['71', 'Snow fall slight'],
+			['73', 'Snow fall moderate'],
+			['75', 'Snow fall heavy intensity'],
+			['77', 'Snow grains'],
+			['80', 'Rain showers slight'],
+			['81', 'Rain showers moderate'],
+			['82', 'Rain showers violent'],
+			['85', 'Snow showers slight'],
+			['86', 'Snow showers heavy'],
+			['95', 'Thunderstorm slight or moderate'],
+			['96', 'Thunderstorm with slight'],
+			['99', 'Thunderstorm with heavy hail'],
+		])
+
 		let city = await loadCity()
 		// let results = city.results ?? []
 		/* get first instance of city from search query */
@@ -30,13 +61,13 @@ module.exports = {
 
 		const dateFormat = todayDate.split('/');
 		const date = dateFormat.reverse().join('-');
-		const time = nowTime.split('.')
+		const currentTime = nowTime.split('.')
 
-		if (time.includes('0')) {
-			time = time[0][1];
+		if (currentTime.includes('0')) {
+			currentTime = currentTime[0][1];
 		}
 
-		const index = parseInt(time);
+		const index = parseInt(currentTime);
 
 		let url = (`https://api.open-meteo.com/v1/forecast?latitude=${latitude}&longitude=${longitude}&hourly=temperature_2m,weathercode,windspeed_10m,winddirection_10m&timezone=auto&start_date=${date}&end_date=${date}`)
 
@@ -46,9 +77,19 @@ module.exports = {
 		}
 
 		let forecastData = await loadForecast();
-		const stringJSON = JSON.stringify(forecastData)
-		// await interaction.reply(`Nama kota: ${cityName} lat: ${latitude}, long: ${longitude}. tanggal = ${date}, jam = ${time}, index = ${index}, type = ${typeof date}`);
-		// await interaction.reply(url)
-		await interaction.reply(stringJSON)
+		const { hourly } = forecastData
+		let stringJSON = JSON.stringify(hourly)
+		const { time, temperature_2m, weathercode, windspeed_10m, winddirection_10m } = hourly;
+		const strWMO = weathercode[index].toString()
+		const rainDescription = '';
+		const rainCode = ['61', '63', '65', '66', '67', '80', '81', '82', '85', '86', '95', '96', '99'];
+
+		if (weather_map.has('3')) {
+			console.log(`${strWMO} pong!`)
+		}
+
+
+		await interaction.deferReply({ ephemeral: false });
+		await interaction.editReply(`${time[index]} ${temperature_2m[index]} ${weathercode[index]} ${windspeed_10m[index]} ${winddirection_10m[index]} \n${stringJSON}`)
 	},
 };
