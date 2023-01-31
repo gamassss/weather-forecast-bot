6eaac0374f190c1399d7b62768eca6bcde0e65a0
gamassss <gamassakbar@gmail.com>
2023-01-31 12:55:04 +0700

    feat(respond): formatted message with embed, (-) timestamp

--- commands/wf-cast.js
+++ commands/wf-cast.js
@@ -8,36 +8,38 @@ module.exports = {
 		.setDescription('Replies weather forecast.')
 		.addStringOption(option =>
 			option
-				.setName('city')
-				.setDescription('The choosen city name')
+				.setName('locations')
+				.setDescription('Choose any location')
 				.setRequired(true)),
 	async execute(interaction) {
-		const cityName = interaction.options.getString('city') ?? 'No city choosen';
+		const cityName = interaction.options.getString('locations') ?? 'No city choosen';
 		// let latitude, longtitude;
 		async function loadCity() {
 			const response = await fetch(`https://geocoding-api.open-meteo.com/v1/search?name=${cityName}`);
 			return response.json();
 		}
-
-		let city = await loadCity()
-		// let results = city.results ?? []
+		// console.log(await loadCity())
+		const city = await loadCity()
+		// console.log(city)
+		const { results } = city ?? []
 		/* get first instance of city from search query */
-		let latitude = city.results[0].latitude
-		let longitude = city.results[0].longitude
-
+		const latitude = results[0].latitude
+		const longitude = results[0].longitude
+		// console.log(`lat: ${latitude}, long: ${longitude}`)
 		const event = new Date();
 		const todayDate = event.toLocaleDateString('en-GB');
 		const nowTime = event.toLocaleTimeString();
 
 		const dateFormat = todayDate.split('/');
 		const date = dateFormat.reverse().join('-');
-		const currentTime = nowTime.split('.')
+		// currentTime untuk menyimpan index
+		const [ curIndex ] = nowTime.split(':')
 
-		if (currentTime.includes('0')) {
-			currentTime = currentTime[0][1];
+		if (curIndex.includes('0')) {
+			curIndex = curIndex[0][1];
 		}
 
-		const index = parseInt(currentTime);
+		const index = parseInt(curIndex);
 
 		let url = (`https://api.open-meteo.com/v1/forecast?latitude=${latitude}&longitude=${longitude}&hourly=temperature_2m,weathercode,windspeed_10m,winddirection_10m&timezone=auto&start_date=${date}&end_date=${date}`)
 
@@ -48,7 +50,6 @@ module.exports = {
 
 		let forecastData = await loadForecast();
 		const { hourly } = forecastData
-		// let stringJSON = JSON.stringify(hourly)
 		const { time, temperature_2m, weathercode, windspeed_10m, winddirection_10m } = hourly;
 		const strWMO = weathercode[index].toString()
 		let rainDescription = '';
@@ -58,14 +59,45 @@ module.exports = {
 		} else {
 			rainDescription = 'Happy no rain day :)'
 		}
+		const [ , jam ] = time[index].split('T')
 
+		const embed = {
+			title: "Weather Forecast" ,
+			color: 0xF96221,
+			thumbnail: {
+				"url": "attachment://weather-icon-for-bot.png"
+			},
+			fields: [
+				{
+					name: "Daily Weather Forecast Update in Your Area",
+					value: "",
+					inline: false
+				},
+				{
+					name: "When is rain?",
+					value: "Two",
+					inline: true,
+				},
+				{
+					name: "Current Temperature",
+					value: "Two",
+					inline: true,
+				},
+				{
+					name:"Wind",
+					value:"Three",
+					inline: true
+				},
+			],
+			"footer": {
+				"text":`By using this service, you agreed to our Terms and Service.`
+			}
+		};
 
-		await interaction.deferReply({ ephemeral: false });
-		await interaction.editReply(`Jam: ${time[index]}\n
-		Suhu: ${temperature_2m[index]}\n
-		CodeWMO: ${weathercode[index]}\n
-		Deskripsi cuaca: ${rainDescription}\n
-		Wind speed: ${windspeed_10m[index]}\n
-		Wind direction:${winddirection_10m[index]}`)
+		// await interaction.deferReply({ ephemeral: false });
+		await interaction.reply({ embeds: [embed], files: [{
+			attachment:'img/weather-icon-for-bot.png',
+			name:'weather-icon-for-bot.png'
+		}] })
 	},
 };
